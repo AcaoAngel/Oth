cc9739f447c95842c7712d9da9ace6fd1c0860aa
angel <angel.acao92@gmail.com>
2021-01-13 11:48:58 +0200

    css found by django

--- Oth_project/settings.py
+++ Oth_project/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'fontawesome-free',
     'Post',
     'Oth_app',
     'Contact',

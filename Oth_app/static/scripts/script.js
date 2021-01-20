b1f976e1c03321b1d44c41aa1fe1b79c7a8d2f10
Lynda <reve2022@hotmail.com>
2021-01-20 16:55:38 +0200

    posti kehitys- ckeditor, script ei toimi

---
+++ Oth_app/static/scripts/script.js
@@ -0,0 +1,7 @@
+
+allowedContent: {
+    img: {
+       attributes: '!src, alt', /* src is required*/
+       styles: 'height, width'
+    }
+}
--- Post/models.py
+++ Post/models.py
@@ -2,15 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 from django.utils import timezone
+from ckeditor.fields import RichTextField
+
+
 
 class Post(models.Model) :
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
 	# profile = models.ForeignKey('users.Profile', on_delete=models.CASCADE)
 	title = models.CharField(max_length = 50)
-	content = models.TextField(default = ' ')#TODO learn about rich text fields: https://www.youtube.com/watch?v=mF5jzSXb1dc
-	img = models.ImageField(upload_to = 'post_img/' , default = 'post_img/default.png')
+	content = RichTextField() #TODO learn about rich text fields: https://www.youtube.com/watch?v=mF5jzSXb1dc
+	img = models.ImageField(upload_to = 'post_img/' , default= 'post_img/default.png')
 	created = models.DateTimeField(default=timezone.now)
-
-
+	active = models.BooleanField(default=True)
 	def __str__(self):
 		return self.title
+		
+
+	

9fb5c75389453b19f901a12510c18962ae554b74
angel <angel.acao92@gmail.com>
2021-01-21 14:12:39 +0200

    before deleting some migrations

--- accounts/models.py
+++ accounts/models.py
@@ -7,30 +7,35 @@ from django.db.models.signals import post_save
 
 
 class Account_value(models.Model):
-    user = models.OneToOneField(User, 
+    user = models.ForeignKey(User, 
                                     on_delete=models.CASCADE,#When user deleted profile should be deleted too
                                     null=False, blank=False)#This line tells that cant be a profile without a user 
     account_value = models.DecimalField(max_digits=11, decimal_places=2)
+    date = models.DateField(null=True, blank=True, default=date.today)
+    
     def __str__(self):
-        return str(self.user) + "--" + str(self.account_value)
+        return f"{self.user}: {self.account_value}: {self.date}"
 
 class Movements(models.Model):
     user = models.ForeignKey(User, 
                                     on_delete=models.CASCADE,#When user deleted profile should be deleted too
                                     null=False, blank=False)#This line tells that cant be a profile without a user 
-    date = models.DateField(default=date.today)
+    date = models.DateField(null=True, blank=True, default=date.today)
     amount = models.DecimalField(max_digits=11, decimal_places=2)
     payee_payer = models.CharField(max_length=50)
     EVENT_CHOICES = [
         ("card", "Card purchase"),
         # ("a_save", "Auto save"), We try using only save, if its necessary wee add this line for autosave like e-possu from nordea
         ("save", "Save"),
-        ("o_transfer","Own transfer"),
+        ("o_transfer_out","Own transfer(Send out)"),
+        ("o_transfer_in","Own transfer(Send in)"),
         ("payment", "Payment transfer"),
         ("deposit", "Deposit"),
     ]
-    event = models.CharField(max_length=10,choices=EVENT_CHOICES)
+    event = models.CharField(max_length=14,choices=EVENT_CHOICES)
     message = models.TextField(default="This is a default text", blank=True)
+    account_value_before = models.DecimalField(max_digits=11, decimal_places=2, help_text="leave empty, generated automatically", default=0)
+    account_value_after = models.DecimalField(max_digits=11, decimal_places=2,  help_text="leave empty, generated automatically", default=0)
 
     def __str__(self):
         info = f"""{self.user.username}
@@ -43,16 +48,45 @@ class Movements(models.Model):
         return info
   
 
-@receiver(post_save, sender=Account_value)#<---indicates that the function is a receiver of a signal: https://adriennedomingus.com/blog/signals-in-django
-def update_account_value(sender, instance, **kwargs):
-    # Account_value.account_value = Account_value.account_value + Movements.amount
-    print("getting in update account_value")
-    print(instance.user.id)
-    editor = Account_value.objects.get(user=instance.user.id)
-    # editor.account_value += instance.amount
-    # print(editor)
-# post_save.connect(update_account_value, sender=Movements)
+@receiver(post_save, sender=Movements)#<-this decorator waits until new movement is created to aply account_value update
+def update_account_value(sender, instance, **kwargs):# instance is the sender objet(Movement)
+    """function for editing a table field after post from a different table
+    in this function after adding a new movement is automatically sent the 
+    signal for updating the account´s related account_value field calculating 
+    the the current amount after paying or receiving money
+    """
 
-# @receiver(post_save, sender=User)#<---indicates that the function is a receiver of a signal: https://adriennedomingus.com/blog/signals-in-django
-# def save_profile_user(sender, instance, **kwargs):
-#     instance.profile.save()
+    editor = Account_value.objects.get(user=instance.user.id)
+    instance.account_value_before = editor.account_value
+    editor.account_value += instance.amount
+    instance.account_value_after = editor.account_value
+    instance.save()
+    editor.save()
+
+# @receiver(post_save, sender=Movements)#<-this decorator waits until new movement is created to aply account_value update
+# def update_account_value(sender, instance, **kwargs):
+#     """function for creating a table field after post from a different table
+#     in this function after adding a new movement is automatically sent the 
+#     signal for creating the account´s new status field calculating 
+#     the the current amount after paying or receiving money
+#     """
+#     pay = ["card", "o_transfer_out", "payment"]
+#     if instance.EVENT_CHOICES in pay:
+#         print("got in event choices")
+#         if instance.amount > 0:
+#             print("number greater the 0")
+#             instance.amount = instance.amount *-1
+#             print("converted number is", instance.amount)
+#             instance.save()
+
+#     user_object = Account_value.objects.get(user=instance.user.id)
+#     print(user_object)
+
+#     print(last_status)
+#     new_value = last_status.account_value + instance.amount,
+#     account_status = Account_value.objects.create(
+#         user_id=Account_value.objects.get(user=instance.user.id, date=Movements.date.max()),
+#         Account_value=new_value,
+#         date = date.today()
+#     )
+
--- testaus.py
+++ testaus.py
@@ -1,20 +1,7 @@
-def lynda(parameter_func):
-    def interior_func():
-        print("summa")
-        parameter_func()
-        print("loppu")
-    return interior_func
-
-@lynda
-def summa():
-    
-    print(5+9)
-
-summa()
+import time
 
 
-@lynda
-def substraction():
-    print(9-5)
 
-substraction()
+ahora = time.strftime("%c")
+## representacion de fecha y hora
+print(time.strftime("%c"))

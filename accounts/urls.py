2253a125267eebbeb7bb94f5304af2e97efaa851
angel <angel.acao92@gmail.com>
2021-02-01 11:51:43 +0200

    added movements list in the account information

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path("movements_form/", views.movements_form, name='movements_form'),
     # path("view_accounts/", views.view_accounts, name='view_accounts'),
     path('view_accounts/', views.view_accounts.as_view(), name="view_accounts"),
-    path('account_detail/<int:id>/', views.account_detail.as_view(), name="account_detail"),
+    # path('account_detail/<int:id>/<int:id>/', views.account_detail.as_view(), name="account_detail"),#id is passed in the clicked link
+    path('account_detail/<int:id>/', views.account_detail, name="account_detail"),#id is passed in the clicked link
       
 ]

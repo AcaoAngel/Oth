307ca78938e83ac6cc5da6da8ac13ff89b78cff0
angel <angel.acao92@gmail.com>
2021-02-05 11:40:08 +0200

    fixed again post rights

--- Post/views.py
+++ Post/views.py
@@ -27,48 +27,52 @@ def post(request , id):
 
 
 
-
-
 def create_post(request):
-	
-    if request.user.is_authenticated and current_user.is_staff == 1:
+    try:
         current_user = User.objects.get(id=request.user.id)
-        if request.method == 'POST':
-        	form = PostForm(data=request.POST, files = request.FILES)
-        	if form.is_valid():
-        		new_form = form.save(commit=False)
-        		print(request.user, type(request.user))
-        		new_form.user = request.user
-        		new_form.save()
-        		messages.success(request, 'Julkaisu on luotu onnistuneesti')
-        		return redirect('/allposts')
-        form = PostForm()
-        return render(request , 'create.html' , {"form": form})
-    else:
+        if request.user.is_authenticated and current_user.is_staff:
+            
+            if request.method == 'POST':
+            	form = PostForm(data=request.POST, files = request.FILES)
+            	if form.is_valid():
+            		new_form = form.save(commit=False)
+            		print(request.user, type(request.user))
+            		new_form.user = request.user
+            		new_form.save()
+            		messages.success(request, 'Julkaisu on luotu onnistuneesti')
+            		return redirect('/allposts')
+            form = PostForm()
+            return render(request , 'create.html' , {"form": form})
+        else:
+        	return render(request, "permisions_denied.html" )
+    except:
     	return render(request, "permisions_denied.html" )
 
 
 
 def edit_post(request , id ):
 	
-    
-    if request.user.is_authenticated and current_user.is_staff == 1:#permisions to create and edit posts, change to is_superuser for su permisions
+    try:
         current_user = User.objects.get(id=request.user.id)
-        post = get_object_or_404(Post, id=id)
-        if request.method == 'GET':
-        	form = PostForm(instance = post)#Get the previous form info
+        if request.user.is_authenticated and current_user.is_staff == 1:#permisions to create and edit posts, change to is_superuser for su permisions
+            current_user = User.objects.get(id=request.user.id)
+            post = get_object_or_404(Post, id=id)
+            if request.method == 'GET':
+            	form = PostForm(instance = post)#Get the previous form info
+            else:
+                form = PostForm(request.POST, files = request.FILES, instance=post)
+                if form.is_valid():
+                    new_form = form.save(commit=False)#when there are fields that must be filled automatically after save we use commit=False
+                    new_form.user = request.user
+                    new_form.save()
+                    return redirect('/allposts')
+          
+            return render(request , 'edit.html', {'form':form})#we pass as content previous form to fill the fields in the edit post html
         else:
-            form = PostForm(request.POST, files = request.FILES, instance=post)
-            if form.is_valid():
-                new_form = form.save(commit=False)#when there are fields that must be filled automatically after save we use commit=False
-                new_form.user = request.user
-                new_form.save()
-                return redirect('/allposts')
-      
-        return render(request , 'edit.html', {'form':form})#we pass as content previous form to fill the fields in the edit post html
-    else:
-	
-        return render(request, "permisions_denied.html")
+        
+            return render(request, "permisions_denied.html")
+    except:
+    	return render(request, "permisions_denied.html")
 
 
 

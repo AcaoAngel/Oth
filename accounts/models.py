2ff46ffde31da4f5f3c5948faa7c1cd0708f4f1d
Lynda <reve2022@hotmail.com>
2021-02-05 11:09:54 +0200

    viimeisia muutoksia

--- accounts/models.py
+++ accounts/models.py
@@ -29,7 +29,7 @@ class Movements(models.Model):
                                     null=False, blank=False)#This line tells that cant be a movement without an account
     date = models.DateField(null=True, blank=True, default=date.today())
     amount = models.DecimalField(max_digits=11, decimal_places=2, help_text="Insert a negative number if you are paying")
-    move_to_account = models.CharField(max_length=100, choices=accounts.functions.accounts_list_for_choices(), blank=True, null=True)
+    move_to_account = models.CharField(max_length=100, choices=accounts.functions.accounts_list_for_choices(), blank=True, null=True, default= None)
     payee_payer = models.CharField(max_length=50, default="default payer", blank=True, null=True)
     event = models.CharField(max_length=14, blank=True, null=True)
     message = models.TextField(default="This is a default text", blank=True)
